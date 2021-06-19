7ccd097f481bb444973737b4d6a5d830f06f6b37
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-06-20 02:04:50 +0530

    heading

---
+++ src/Content/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Content.css'
+
+const Content = () => {
+    return (
+        <div>
+            <h1>Why Data Science Sdot Data <br></br> Science for everyone</h1>
+        </div>
+    )
+}
+
+export default Content
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Content from './Content/Content';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Content />
   </React.StrictMode>,
   document.getElementById('root')
 );
